78802d8cf7349112e255d728c562172530415e15
Yordano Contreras <yordano@MacBook-Pro-de-Yordano.local>
2023-01-17 07:38:09 +0100

    first commit : install - vue bootstrap tailwindcss symfony6 php8

---
+++ assets/home.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import App from './pages/home';
+
+createApp(App).mount('#app');
---
+++ assets/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <Banner/>
+  <Form/>
+</div>
+</template>
+<script>
+import Form from "../components/Form"
+import Banner from "../components/Banner"
+export default {
+  name: 'home',
+  data() {
+    return {};
+  },
+  components : {Banner,Form}
+}
+</script>
